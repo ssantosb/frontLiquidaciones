031b86ec60ae11c9c8e5136d2e3ba3c7c7549b28
Nicolas Urrego <n.urrego@uniandes.edu.co>
2021-07-18 00:33:02 -0400

    TQM

--- src/app/components/pionero-actualizar/pionero-actualizar.component.ts
+++ src/app/components/pionero-actualizar/pionero-actualizar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PrecioPionero } from 'src/app/shared/models/precioPionero';
 
 @Component({
   selector: 'app-pionero-actualizar',
@@ -7,25 +8,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PioneroActualizarComponent implements OnInit {
 
+  loadedFile:File
+  act:PrecioPionero[] = [];
+  errorNoFile:boolean = false;
+  errorFileExt:boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  
   public changeListener(files: FileList){
     console.log(files);
     if(files && files.length > 0) {
-       let file : File = files.item(0); 
-         console.log(file.name);
-         console.log(file.size);
-         console.log(file.type);
-         let reader: FileReader = new FileReader();
-         reader.readAsText(file);
-         reader.onload = (e) => {
-            let csv: string = reader.result as string;
-            console.log(csv);
-         }
+      this.loadedFile = files.item(0); 
+    }
+  }
+
+  loadFile():void{
+    if(this.loadedFile == null){
+      this.errorNoFile = true;
+      console.log("NO")  
+    }
+    /* else if(this.loadedFile.type != "csv"){
+      this.errorFileExt = true;
+      console.log("EXT")  
+    } */
+    else{
+      let reader: FileReader = new FileReader();
+      reader.readAsText(this.loadedFile);
+      reader.onload = (e) => {
+        let csv: string = reader.result as string;
+        let pionerosTxt:string[] = csv.split("\n")
+        let array:PrecioPionero[] = [];
+        pionerosTxt.forEach(function (pioTxt) {
+          let pioneroInfo:string[] = pioTxt.split(",")
+          let precioAct: PrecioPionero = {
+            idConcepto: <number><unknown>pioneroInfo[0],
+            precio: <number><unknown>pioneroInfo[1],
+            vigencia: pioneroInfo[2]
+          };
+          console.log(precioAct);
+          array.push(precioAct);
+        }); 
+        this.act = array
       }
+      console.log(this.act) 
+    } 
+  }
+
+  getAct():PrecioPionero[]{
+    return this.act;
   }
 
 }
